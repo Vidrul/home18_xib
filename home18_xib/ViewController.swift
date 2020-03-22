8e49c2d404a0fc8901bc99f2f9911707dc7c6f47
Vidrul <Vidrul@ugithub.com>
2020-03-22 12:38:39 +0300

    Initial Commit

---
+++ home18_xib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  home18_xib
+//
+//  Created by David Saley on 3/22/20.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
