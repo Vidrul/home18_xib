473264a51edcad3f51331091995e2557f28663b9
Vidrul <Vidrul@ugithub.com>
2020-03-22 16:37:17 +0300

    ok!

---
+++ home18_xib/FlagViewController.swift
@@ -0,0 +1,55 @@
+
+import UIKit
+
+class FlagViewController: UIView {
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
+    var clickHandler: (() -> Void)?
+    
+    @IBAction func redButtonDidClick(_ sender: UIButton) {
+        clickHandler?()
+        UIView.animate(withDuration: 1.0) {
+            self.mainView.backgroundColor = UIColor.init(displayP3Red: 1.0 , green: 0, blue: 0, alpha: 1.0)
+        }
+        
+    }
+    
+    @IBAction func blueButtonDidClick(_ sender: UIButton) {
+        clickHandler?()
+        UIView.animate(withDuration: 1.0) {
+            self.mainView.backgroundColor = UIColor.init(displayP3Red: 0, green: 0, blue: 1.0 , alpha: 1.0)
+        }
+    }
+    
+    @IBAction func greenButtonDidClick(_ sender: UIButton) {
+        clickHandler?()
+        UIView.animate(withDuration: 1.0) {
+            self.mainView.backgroundColor = UIColor.init(displayP3Red: 0, green: 1.0, blue: 0, alpha: 1.0)
+        }
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("FlagViewController", owner: self, options: nil)
+        self.addSubview(mainView)
+        mainView.frame = self.bounds
+        mainView.autoresizingMask = [.flexibleHeight,.flexibleWidth]
+        mainView.backgroundColor = .black
+    }
+    
+}
--- home18_xib/ViewController.swift
+++ home18_xib/ViewController.swift
@@ -1,18 +1,12 @@
-//
-//  ViewController.swift
-//  home18_xib
-//
-//  Created by David Saley on 3/22/20.
-//  Copyright © 2020 David. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: FlagViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
